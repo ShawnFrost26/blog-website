4ed4380ca8227fa42242cc94128c7f9724df2514
ShawnFrost26 <sb262202@gmail.com>
2023-07-01 21:43:24 +0530

    installed required dependencies, added .env file, index.js file, added the boiler code to it

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT;
+
+const DB_URI = process.env.DB_URI;
+mongoose
+.connect(`${DB_URI}`)
+.then(()=> {console.log("Connected to DB at", DB_URI)})
+.catch((e)=> {console.log("Failed to connect to DB", e)})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Started to listen on PORT: ${PORT}`);
+});
