dc4797d87a47d35f201a672a730f145884957f7a
ShawnFrost26 <sb262202@gmail.com>
2023-07-02 10:41:51 +0530

    defined the user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: (value) => validator.isEmail(value),
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
